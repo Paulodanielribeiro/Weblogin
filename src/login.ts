a58bc8483b9d4a55f0dd2baddc592925f6024f30
paulo <paulomoraes@gamil.com>
2022-05-17 20:25:30 -0400

    Novo

---
+++ src/login.ts
@@ -0,0 +1,12 @@
+import renderLoginForm from './components/LoginForm'
+import './styles.css'
+
+const app = <HTMLDivElement>document.querySelector('#app')
+renderLoginForm(app)
+
+const createUserLink =
+    <HTMLAnchorElement>document.createElement('a')
+createUserLink.id = 'redirect-link'
+createUserLink.innerText = 'Clique aqui para criar uma nova conta'
+createUserLink.href = 'createuser.html'
+app.insertAdjacentElement('beforeend', createUserLink)
